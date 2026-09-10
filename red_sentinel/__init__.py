d804e16bc2f8dfa42245a879e6d291315546a9a2
SKIZZOO <62386847+SKIZZOO@users.noreply.github.com>
2026-09-10 23:57:27 +0300

    Load OAuth setup helper

--- red_sentinel/__init__.py
+++ red_sentinel/__init__.py
@@ -1,4 +1,6 @@
 from .red_sentinel import RedSentinel
+from .oauth_setup import SentinelOAuthSetup
 
 async def setup(bot):
     await bot.add_cog(RedSentinel(bot))
+    await bot.add_cog(SentinelOAuthSetup(bot))
