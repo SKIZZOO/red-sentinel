378d0d55dac2bc98a27c9079fad7729fe66b93e5
SKIZZOO <62386847+SKIZZOO@users.noreply.github.com>
2026-09-11 06:49:30 +0300

    Make Server Control use full fluid desktop canvas

--- dashboard/visuals.js
+++ dashboard/visuals.js
@@ -24,5 +24,6 @@
   css('server-viewport-fit-css','./server-viewport-fit.css?v=20260911-1');
   css('server-fullpage-css','./server-fullpage.css?v=20260911-1');
   css('server-full-width-css','./server-full-width.css?v=20260911-1');
+  css('layout-fluid-css','./layout-fluid.css?v=20260911-1');
   load('server-fix-js','script','./server-fix.js?v=20260911-1');load('platform-icons-js','script','./platform-icons.js?v=20260911-1');
 })();
